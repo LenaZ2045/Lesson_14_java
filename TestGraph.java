7feb2a5cc64f2d12dadd0ecd28c7ed515ff97f19
Lena Zimin <Bogypesn@gmail.com>
2023-11-15 15:36:41 -0500

    from JavaPro on 11-15-2023 added to Lesson14 file name TestGraph

---
+++ TestGraph.java
@@ -0,0 +1,20 @@
+package com.telran.org.lesson14;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class TestGraph {
+    public static void main(String[] args) {
+        List<Edge> edges = new ArrayList<>();
+
+        //vertex = 0; 0-1=1, 1-2=2, 2-2=2
+        edges.add(new Edge(0, 1));
+        edges.add(new Edge(1, 2));
+        edges.add(new Edge(2, 0));
+        edges.add(new Edge(2, 1));
+        edges.add(new Edge(3, 2));
+
+        Graph graph = new Graph(edges);
+        Util.printGraph(graph);
+    }
+}
